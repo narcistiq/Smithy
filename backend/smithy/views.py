396ee8b8f85db4aa5274012c2f492a110e631801
smhcristian <cgarciasalinas@ufl.edu>
2025-12-02 18:34:14 -0500

    Connected front and back ends with endpoints ; views.py changed to public

--- backend/smithy/views.py
+++ backend/smithy/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -12,6 +13,7 @@ from typing import Optional
 
 @csrf_exempt
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register(request):
     """Register a new user"""
     username = request.data.get('username')
@@ -56,6 +58,7 @@ def register(request):
 
 @csrf_exempt
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
     """Login user and return token"""
     username = request.data.get('username')
