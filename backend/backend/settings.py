32494de9e95635955a8a74833a168f820313a14d
tiffh <tiffhuang321@gmail.com>
2025-12-10 13:51:06 -0500

    minor bug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",  # CORS - must be at top
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
